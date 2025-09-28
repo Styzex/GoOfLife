950d7bd6bb6addca26a7e45824329c385bb3d655
Viktor Paraj <viktor.paraj@gmail.com>
2025-09-28 13:26:18 +0200

    Created grid and added error handling

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/charmbracelet/log"
+)
+
+const width = 10
+const height = 10
 
 func main() {
-	fmt.Println("Hello Life!")
+	grid := make([][]bool, height)
+	for i := range grid {
+		grid[i] = make([]bool, width)
+	}
+
+	value, err := Check_live(1, 20, grid)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	print(value)
+}
+
+func Check_live(x int, y int, grid [][]bool) (bool, error) {
+	if x > width {
+		err := fmt.Errorf("invalid width  %d, input width is bigger than the height of the grid", x)
+		return false, err
+	} else if y > height {
+		err := fmt.Errorf("invalid height %d, input height is bigger than the height of the grid", y)
+		return false, err
+	} else {
+		if grid[x][y] {
+			return true, nil
+		}
+		return false, nil
+	}
 }
